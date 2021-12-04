61a36e76dd6abef122e2ce24653f404ea10bded7
anton <langbru2@uwm.edu>
2021-12-03 20:28:39 -0600

    debug

--- CapstoneProject/settings.py
+++ CapstoneProject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g-w1&8(e8h^rm3g_$8vh8s-%^+c@fr#zj5=5wrp2_h*&uce%%u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = []
